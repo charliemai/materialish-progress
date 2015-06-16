94827c797bf1ededde823f5607e6f5e290b0be2b
charliemai <charlie@taiwanyo.com.tw>
2015-06-16 10:40:18 +0800

    From gradle project to ADT project

---
+++ app/src/main/gen/com/pnikosis/materialishprogress/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pnikosis.materialishprogress.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
